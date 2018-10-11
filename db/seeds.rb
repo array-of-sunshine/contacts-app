728182a73bcbe7c61a8da126301ae82f844d4f47
Brian <riceb53@gmail.com>
2018-10-10 21:03:26 -0500

    add faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do
+  p Faker::Name.name
+  # contact3 = Contact.new(first_name: Faker::Name.name, last_name: "johnson", email: "aosdjfa", phone_number: "097654")
+  # contact3.save
+end
+# contact3 = Contact.new(first_name: "jason", last_name: "williams", email: "somasdhf", phone_number: "2345678")
+# contact3.save
+
+
+# contact3 = Contact.new(first_name: "jason", last_name: "williams", email: "somasdhf", phone_number: "2345678")
+# contact3.save
+
